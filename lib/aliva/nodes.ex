ba09d11910eec66083499a5f67100bec75e91bc4
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2020-10-29 12:59:33 +0500

    Changing variable naming scheme

--- lib/aliva/nodes.ex
+++ lib/aliva/nodes.ex
@@ -2,21 +2,25 @@ defmodule Aliva.Nodes do
   defstruct %{}
 
   def addNode(ip, id, socket, type, peers) do
-    myPeerStruct = generatePeerStruct(id, type, socket)
-    newList = mergeLists(peers, myPeerStruct)
-    ipKey = convertIpToAtom(ip);
-    Map.put(%Nodes{}, ipKey,  newList)
+    my_peer_struct = generate_peer_struct(id, type, socket)
+    new_list = merge_lists(peers, my_peer_struct)
+    ip_key = convert_ip_to_atom(ip);
+    Map.put(%Nodes{}, ip_key,  new_list)
+g
+    %Nodes{}
+    |> genrate_ip(ip_key,new_list)
+    |>
   end
 
-  def generatePeerStruct(id, socket, type) do
+  def generate_peer_struct(id, socket, type) do
     %{id: id, type: type, connection: socket}
   end
 
-  def mergeLists(peers, myPeerStruct) do
-    Enum.concat(peers,[myPeerStruct] )
+  def merge_lists(peers, my_peer_struct) do
+    Enum.concat(peers,[my_peer_struct] )
   end
 
-  def convertIpToAtom(ip) do
+  def convert_ip_to_atom(ip) do
     String.to_atom(ip)
   end
 end
