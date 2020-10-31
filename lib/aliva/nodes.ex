842e52498389a84dfa4c63e48104e52f889101a7
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2020-10-31 23:23:50 +0500

    Working on data structure

--- lib/aliva/nodes.ex
+++ lib/aliva/nodes.ex
@@ -1,28 +1,29 @@
 defmodule Aliva.Nodes do
-  defstruct [nodes: %{ip: [%{id: "", type: "", connection: ""}]}]
+  defstruct [my_nodes: %{}]
 
-  # %Main{nodes: %{
-	# "1.1.1.1": [%{connection: "myCon", id: "1", type: "Master"},%{connection: "myCon", id: "2", type: "Child"}],
-	# "2.2.2.2": [%{connection: "myCon", id: "1", type: "Master"},%{connection: "myCon", id: "2", type: "Child"}]
-	# }}
+    # %Main{nodes: %{
+  	# "1.1.1.1": [%{connection: "myCon", id: "1", type: "Master"},%{connection: "myCon", id: "2", type: "Child"}],
+  	# "2.2.2.2": [%{connection: "myCon", id: "1", type: "Master"},%{connection: "myCon", id: "2", type: "Child"}]
+  	# }}
 
-  def addNode(ip, id, socket, type, peers) do
-    new_list = generate_peer_struct(id, type, socket)
-    |> merge_lists(peers)
-    # ip_key = convert_ip_to_atom(ip)
-    updated_map = %{%Nodes{}.nodes | "#{ip}": new_list }
-    Map.put(%Nodes{}, :nodes,  updated_map)
-  end
+    def addNode(ip, id, socket, type, peers) do
+      new_list = generate_peer_struct(id, type, socket)
+      |> merge_lists(peers)
+      # ip_key = convert_ip_to_atom(ip)
+      # updated_map = %{%Main{}.my_nodes | "#{ip}": new_list }
+      updated_map = Map.put(%Main{}.my_nodes, "#{ip}", new_list)
+      Map.put(%Main{}, :my_nodes,  updated_map)
+    end
 
-  def generate_peer_struct(id, socket, type) do
-    %{id: id, type: type, connection: socket}
-  end
+    def generate_peer_struct(id, socket, type) do
+      %{id: id, type: type, connection: socket}
+    end
 
-  def merge_lists(my_peer_struct, peers) do
-    Enum.concat(peers,[my_peer_struct] )
-  end
+    def merge_lists(my_peer_struct, peers) do
+      Enum.concat(peers,[my_peer_struct] )
+    end
 
-  def convert_ip_to_atom(ip) do
-    String.to_atom(ip)
-  end
+    def convert_ip_to_atom(ip) do
+      String.to_atom(ip)
+    end
 end
--- lib/aliva_web/channels/NodeChannel.ex
+++ lib/aliva_web/channels/NodeChannel.ex
@@ -1,7 +1,13 @@
 defmodule AlivaWeb.UserChannel do
+  import Aliva.Nodes
+  import Ecto
   use AlivaWeb, :channel
 
   def join("initial:peer", message, socket) do
+    id = Ecto.UUID.generate
+    # addNode(ip, id, socket, type, peers)
+    Aget.start
+    addNode("1.1.1.1", id, socket, "MASTER", []);
     IO.inspect(socket)
   end
 
