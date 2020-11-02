fc9d461592eeda68922d033858777fcd77d03b37
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2020-11-02 11:29:06 +0500

    Getting Ip peer list from agent

--- lib/aliva/nodes.ex
+++ lib/aliva/nodes.ex
@@ -7,24 +7,33 @@ defmodule Aliva.Nodes do
     # }}
 
     def start_link(initial_state) do
+      IO.inspect("Agent Started...........")
       Agent.start_link(fn -> initial_state end, name: __MODULE__)
     end
 
     def addNode(ip, id, socket, type) do
-      peers = get_peers(ip)
-      new_list = generate_peer_struct(id, type, socket)
-      |> merge_lists(peers)
-      updated_map = Map.put(%Aliva.Nodes{}.my_nodes, "#{ip}", new_list)
-      Agent.update(__MODULE__, fn _ -> updated_map end)
+      IO.inspect("#{ip} ip")
+      IO.inspect("#{id} id")
+      IO.inspect("#{socket} socket")
+      IO.inspect("#{type} type")
+      IO.inspect(get_peers(ip), label: "Get Peers")
+      case get_peers(ip) do
+        nil -> IO.inspect("There is no peer at #{ip}")
+      end
+      # new_list = generate_peer_struct(id, type, socket)
+      # |> merge_lists(peers)
+      # updated_map = Map.put(%Aliva.Nodes{}.my_nodes, "#{ip}", new_list)
+      # Agent.update(__MODULE__, fn _ -> updated_map end)
     end
 
     def get_peers(ip) do
-      get_all_node_list()
+
+      get_all_node_map()
       |> get_peers(ip)
     end
 
-    def get_peers(nodes_list, ip) do
-      nodes_list.my_nodes["#{ip}"]
+    def get_peers({_, ips_map}, ip) do
+      Map.get(ips_map, ip)
     end
 
     def generate_peer_struct(id, socket, type) do
@@ -38,7 +47,7 @@ defmodule Aliva.Nodes do
     def convert_ip_to_atom(ip) do
       String.to_atom(ip)
     end
-    def get_all_node_list do
+    def get_all_node_map do
       Agent.get(__MODULE__, fn nodes_list -> nodes_list end)
     end
 end
--- lib/aliva_web/channels/node_channel.ex
+++ lib/aliva_web/channels/node_channel.ex
@@ -5,7 +5,7 @@ defmodule AlivaWeb.NodeChannel do
   def join("initial:peer",_message, socket) do
     IO.inspect("Channel is joined by peer")
     id = Ecto.UUID.generate
-    # addNode("1.1.1.1", id, socket, "MASTER");
+    addNode("1.1.1.1", id, "socket", "MASTER");
     # addNode("1.1.1.1", id, socket, "CHILD");
     # addNode("1.1.1.1", id, socket, "CHILD");
     {:ok, %{}, socket}
