eecfe0a6bf8d99ee48f407a8c2505cb64ea9a95c
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2020-11-03 15:47:44 +0500

    Getting peer on connection in frontend

--- assets/js/Components/home.js
+++ assets/js/Components/home.js
@@ -6,6 +6,8 @@ import { configureChannel } from "../socket";
 class Home extends React.Component {
   state = {
     ip: "",
+    id: "",
+    type: "",
     peers: [],
   };
   constructor(props) {
@@ -14,9 +16,16 @@ class Home extends React.Component {
   async componentDidMount() {
     // configureChannel();
     const { channel, socket } = await configureChannel();
+    const componentThis = this;
     channel
       .join()
-      .receive("ok", ({ peers }) => this.setState({ peers }))
+      .receive("ok", async ({ peers, id, type }) => {
+        const ip = await getMyIp();
+        this.setState({ peers, id, type, ip }, () => {
+          this.newNodeListener(channel);
+          this.sendBroadcast(channel);
+        });
+      })
       .receive("error", ({ reason }) => {
         alert("Something wrong with socket");
         console.log("failed join", reason);
@@ -24,8 +33,19 @@ class Home extends React.Component {
       .receive("timeout", () => {
         alert("Networking issue. Still waiting....");
       });
-    console.log(await getMyIp());
   }
+
+  newNodeListener = (channel) => {
+    const { ip } = this.state;
+    channel.on(`initial:new_${ip}`, function (data) {
+      console.log(`initial:new_${ip} `, data);
+    });
+  };
+
+  sendBroadcast = (channel) => {
+    const { ip, id } = this.state;
+    channel.push("initial:boadcast_new_node", { ip, id });
+  };
 
   render() {
     return (
--- lib/aliva_web/channels/node_channel.ex
+++ lib/aliva_web/channels/node_channel.ex
@@ -10,8 +10,25 @@ defmodule AlivaWeb.NodeChannel do
     socket = assign(socket, :ip , ip)
     socket = Map.update(socket, :id, nil, fn _value -> id end)
     peers = get_all_peers_list(ip)
+    count = Enum.count(peers)
     add_node(ip, id, socket);
-    {:ok, %{peers: peers}, socket}
+    # Broadcast this node to all peers
+    if count > 0 do
+      {:ok, %{peers: peers, id: id, type: "CHILD"}, socket}
+    else
+      {:ok, %{peers: peers, id: id, type: "MASTER"}, socket}
+    end
+  end
+
+  def handle_in("initial:boadcast_new_node", %{"ip" => ip, "id" => id}, socket) do
+    peers = get_all_peers_list(ip)
+    count = Enum.count(peers)
+    if count > 1 do
+      broadcast(socket, "initial:new_#{ip}", %{type: "CHILD", id: id})
+    else
+      broadcast(socket, "initial:new_#{ip}", %{type: "MASTER", id: id})
+    end
+    {:noreply, socket}
   end
 
   def terminate(_reason, socket) do
