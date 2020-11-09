3d0d8edbc64341d977b2334688a6864e5946b8e2
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2020-11-09 16:20:31 +0500

    Update node_channel.ex

--- lib/aliva_web/channels/node_channel.ex
+++ lib/aliva_web/channels/node_channel.ex
@@ -7,7 +7,6 @@ defmodule AlivaWeb.NodeChannel do
     is_same = is_from_same_machine(ip, machine_id)
     peers_list = get_all_peers_list(ip)
     peers_count = Enum.count(peers_list)
-
     if is_same do
       {:ok, %{lan_peers: nil}, socket}
     else
@@ -26,12 +25,10 @@ defmodule AlivaWeb.NodeChannel do
         socket
       ) do
     is_same = is_from_same_machine(ip, machine_id)
-
     if is_same do
       {:noreply, socket}
     else
       socket = assign(socket, :type, type)
-
       if type == "MASTER" do
         handle_master_creation(ip, machine_id, type, socket)
       else
@@ -39,11 +36,11 @@ defmodule AlivaWeb.NodeChannel do
         handle_child_creation(peers_list, ip, machine_id, type, socket)
         broadcast(socket, "web:new_#{ip}", %{type: type, machine_id: machine_id, ip: ip})
       end
-
       socket = Map.update(socket, :id, machine_id, fn _value -> machine_id end)
       {:noreply, socket}
     end
   end
+
 
   def handle_in(
         "web:send_offer_to_master",
@@ -62,13 +59,11 @@ defmodule AlivaWeb.NodeChannel do
     broadcast(socket, "web:remove_#{ip}", %{machine_id: machine_id_to_remove, ip: ip})
     [master_node | _tail] = get_master_node(ip)
     master_node_id = Map.get(master_node, :machine_id)
-
     if type == "MASTER" do
       peers_list = get_all_peers_list_exclude_master(ip)
       broadcast(socket, "web:update_master_in_child#{ip}", %{machine_id: master_node_id, ip: ip})
       broadcast(socket, "web:make_me_master_#{master_node_id}", %{ip: ip, lan_peers: peers_list})
     end
-
     {:ok, %{}, socket}
   end
 end
