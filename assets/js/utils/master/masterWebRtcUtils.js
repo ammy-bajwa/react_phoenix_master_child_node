e49e576667e12e759e2ebe975c5b3aaaffbf5bc4
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2020-11-19 16:16:20 +0500

    Without ice servers and negotiation

--- assets/js/utils/master/masterWebRtcUtils.js
+++ assets/js/utils/master/masterWebRtcUtils.js
@@ -1,32 +1,32 @@
 export const masterCreateWebRtcConObj = (channel, ip, masterId, childId) => {
   const peerConnection = new RTCPeerConnection({
     iceServers: [
-      {
-        urls: ["stun:avm4962.com:3478", "stun:avm4962.com:5349"],
-      },
-      { urls: ["stun:ss-turn1.xirsys.com"] },
-      {
-        username: "TuR9Us3r",
-        credential:
-          "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
-        urls: [
-          "turn:avm4962.com:3478?transport=tcp",
-          "turn:avm4962.com:5349?transport=tcp",
-        ],
-      },
-      {
-        username:
-          "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
-        credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
-        urls: [
-      "turn:ss-turn1.xirsys.com:80?transport=udp",
-          "turn:ss-turn1.xirsys.com:3478?transport=udp",
-      "turn:ss-turn1.xirsys.com:80?transport=tcp",
-      "turn:ss-turn1.xirsys.com:3478?transport=tcp",
-      "turns:ss-turn1.xirsys.com:443?transport=tcp",
-      "turns:ss-turn1.xirsys.com:5349?transport=tcp",
-        ],
-      },
+      // {
+      //   urls: ["stun:avm4962.com:3478", "stun:avm4962.com:5349"],
+      // },
+      // { urls: ["stun:ss-turn1.xirsys.com"] },
+      // {
+      //   username: "TuR9Us3r",
+      //   credential:
+      //     "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
+      //   urls: [
+      //     "turn:avm4962.com:3478?transport=tcp",
+      //     "turn:avm4962.com:5349?transport=tcp",
+      //   ],
+      // },
+      // {
+      //   username:
+      //     "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
+      //   credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
+      //   urls: [
+      // "turn:ss-turn1.xirsys.com:80?transport=udp",
+      //     "turn:ss-turn1.xirsys.com:3478?transport=udp",
+      // "turn:ss-turn1.xirsys.com:80?transport=tcp",
+      // "turn:ss-turn1.xirsys.com:3478?transport=tcp",
+      // "turns:ss-turn1.xirsys.com:443?transport=tcp",
+      // "turns:ss-turn1.xirsys.com:5349?transport=tcp",
+      //   ],
+      // },
     ],
   });
   // const peerConnection = new RTCPeerConnection({ iceServers: [] });
@@ -79,20 +79,20 @@ export const masterCreateWebRtcConObj = (channel, ip, masterId, childId) => {
     };
   };
 
-  peerConnection.onnegotiationneeded = async () => {
-    console.log("NEGOTIATION MASTER");
-    const offerForChild = await peerConnection.createOffer();
-    console.log("MASTER createOffer: ", offerForChild);
-    await peerConnection.setLocalDescription(offerForChild);
-    console.log("MASTER setLocalDescription Offer");
-    channel.push(`web:send_offer_to_child`, {
-      child_id: childId,
-      offer_for_child: JSON.stringify(offerForChild),
-      master_id: masterId,
-      ip,
-    });
-    console.log("MASTER Send Offer");
-  };
+  // peerConnection.onnegotiationneeded = async () => {
+  //   console.log("NEGOTIATION MASTER");
+  //   const offerForChild = await peerConnection.createOffer();
+  //   console.log("MASTER createOffer: ", offerForChild);
+  //   await peerConnection.setLocalDescription(offerForChild);
+  //   console.log("MASTER setLocalDescription Offer");
+  //   channel.push(`web:send_offer_to_child`, {
+  //     child_id: childId,
+  //     offer_for_child: JSON.stringify(offerForChild),
+  //     master_id: masterId,
+  //     ip,
+  //   });
+  //   console.log("MASTER Send Offer");
+  // };
 
   const dataChannel = createDataChannel(peerConnection);
   document.querySelector("#dataChannelMaster").addEventListener("click", () => {
