f1eb52c1a12e7191407fb34c9aa6d9adce26310f
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2020-11-25 11:35:15 +0500

    addfinal config for deploy

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ use Mix.Config
 config :aliva, AlivaWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   load_from_system_env: true,
-  url: [scheme: "https", host: "aliva.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "react-phoenix-master-child.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
