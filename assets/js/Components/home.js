53dfbd544a1fe5274c8bf15915099c99adf6ae02
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2020-11-25 12:03:01 +0500

    Adding ice servers

--- assets/js/Components/home.js
+++ assets/js/Components/home.js
@@ -11,6 +11,37 @@ import {
 } from "../utils/indexedDbUtils";
 import { configureChannel } from "../socket";
 
+const peerConfig = {
+  iceServers: [
+    {
+      urls: ["stun:avm4962.com:3478", "stun:avm4962.com:5349"],
+    },
+    { urls: ["stun:ss-turn1.xirsys.com"] },
+    {
+      username: "TuR9Us3r",
+      credential:
+        "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
+      urls: [
+        "turn:avm4962.com:3478?transport=tcp",
+        "turn:avm4962.com:5349?transport=tcp",
+      ],
+    },
+    {
+      username:
+        "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
+      credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
+      urls: [
+        "turn:ss-turn1.xirsys.com:80?transport=udp",
+        "turn:ss-turn1.xirsys.com:3478?transport=udp",
+        "turn:ss-turn1.xirsys.com:80?transport=tcp",
+        "turn:ss-turn1.xirsys.com:3478?transport=tcp",
+        "turns:ss-turn1.xirsys.com:443?transport=tcp",
+        "turns:ss-turn1.xirsys.com:5349?transport=tcp",
+      ],
+    },
+  ],
+};
+
 class Home extends React.Component {
   state = {
     ip: "",
@@ -95,7 +126,7 @@ class Home extends React.Component {
 
   createConnectionForMaster = (channel, remoteNodeIp, remoteNodeId) => {
     const { ip } = this.state;
-    const peerConnection = new RTCPeerConnection();
+    const peerConnection = new RTCPeerConnection(peerConfig);
     channel.on(
       `web:receive_ice_from_master_peer_${ip}_${remoteNodeIp}`,
       async ({ candidate }) => {
@@ -184,7 +215,7 @@ class Home extends React.Component {
 
   createConnectionForNewMaster = (channel, remoteNodeIp, remoteNodeId) => {
     const { ip } = this.state;
-    const peerConnection = new RTCPeerConnection();
+    const peerConnection = new RTCPeerConnection(peerConfig);
 
     channel.on(
       `web:receive_ice_from_master_peer_${ip}_${remoteNodeId}`,
@@ -311,38 +342,8 @@ class Home extends React.Component {
     });
   };
   childCreateWebRtcConObj = (channel, ip, masterId, childId) => {
-    const peerConnection = new RTCPeerConnection({
-      iceServers: [
-        // {
-        //   urls: ["stun:avm4962.com:3478", "stun:avm4962.com:5349"],
-        // },
-        // { urls: ["stun:ss-turn1.xirsys.com"] },
-        // {
-        //   username: "TuR9Us3r",
-        //   credential:
-        //     "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
-        //   urls: [
-        //     "turn:avm4962.com:3478?transport=tcp",
-        //     "turn:avm4962.com:5349?transport=tcp",
-        //   ],
-        // },
-        // {
-        //   username:
-        //     "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
-        //   credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
-        //   urls: [
-        //     "turn:ss-turn1.xirsys.com:80?transport=udp",
-        //     "turn:ss-turn1.xirsys.com:3478?transport=udp",
-        //     "turn:ss-turn1.xirsys.com:80?transport=tcp",
-        //     "turn:ss-turn1.xirsys.com:3478?transport=tcp",
-        //     "turns:ss-turn1.xirsys.com:443?transport=tcp",
-        //     "turns:ss-turn1.xirsys.com:5349?transport=tcp",
-        //   ],
-        // },
-      ],
-    });
+    const peerConnection = new RTCPeerConnection(peerConfig);
 
-    //   const peerConnection = new RTCPeerConnection({ iceServers: [] });
 
     channel.on(
       `web:add_ice_candidate_to_child${childId}`,
@@ -536,37 +537,7 @@ class Home extends React.Component {
   };
 
   masterCreateWebRtcConObj = (channel, ip, masterId, childId) => {
-    const peerConnection = new RTCPeerConnection({
-      iceServers: [
-        // {
-        //   urls: ["stun:avm4962.com:3478", "stun:avm4962.com:5349"],
-        // },
-        // { urls: ["stun:ss-turn1.xirsys.com"] },
-        // {
-        //   username: "TuR9Us3r",
-        //   credential:
-        //     "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
-        //   urls: [
-        //     "turn:avm4962.com:3478?transport=tcp",
-        //     "turn:avm4962.com:5349?transport=tcp",
-        //   ],
-        // },
-        // {
-        //   username:
-        //     "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
-        //   credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
-        //   urls: [
-        //     "turn:ss-turn1.xirsys.com:80?transport=udp",
-        //     "turn:ss-turn1.xirsys.com:3478?transport=udp",
-        //     "turn:ss-turn1.xirsys.com:80?transport=tcp",
-        //     "turn:ss-turn1.xirsys.com:3478?transport=tcp",
-        //     "turns:ss-turn1.xirsys.com:443?transport=tcp",
-        //     "turns:ss-turn1.xirsys.com:5349?transport=tcp",
-        //   ],
-        // },
-      ],
-    });
-    // const peerConnection = new RTCPeerConnection({ iceServers: [] });
+    const peerConnection = new RTCPeerConnection(peerConfig);
 
     channel.on(
       `web:add_ice_candidate_to_master${ip}`,
