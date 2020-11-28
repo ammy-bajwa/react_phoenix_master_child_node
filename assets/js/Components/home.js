ec961125476fb9dcd63a5ec0c23083994eaf10a1
ammy-bajwa <amirsadiqalisaifi@gmail.com>
2020-11-29 01:50:09 +0500

    Only one ice

--- assets/js/Components/home.js
+++ assets/js/Components/home.js
@@ -51,79 +51,92 @@ class Home extends React.Component {
     messagesFromLanMasterPeer: [],
     messagesFromChildsPeers: [],
     iceConfigs: [
-      { iceServers: [] },
-      {
-        iceServers: [
-          {
-            urls: ["stun:avm4962.com:3478", "stun:avm4962.com:5349"],
-          },
-          { urls: ["stun:ss-turn1.xirsys.com"] },
-        ],
-      },
-      {
-        iceServers: [
-          {
-            username: "TuR9Us3r",
-            credential:
-              "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
-            urls: ["turn:avm4962.com:3478", "turn:avm4962.com:5349"],
-          },
-        ],
-      },
-      {
-        iceServers: [
-          {
-            username: "TuR9Us3r",
-            credential:
-              "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
-            urls: [
-              "turn:avm4962.com:3478?transport=udp",
-              "turn:avm4962.com:5349?transport=udp",
-            ],
-          },
-        ],
-      },
-      {
-        iceServers: [
-          {
-            username: "TuR9Us3r",
-            credential:
-              "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
-            urls: [
-              "turn:avm4962.com:3478?transport=tcp",
-              "turn:avm4962.com:5349?transport=tcp",
-            ],
-          },
-        ],
-      },
-      {
-        iceServers: [
-          {
-            username:
-              "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
-            credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
-            urls: [
-              "turn:ss-turn1.xirsys.com:80?transport=udp",
-              "turn:ss-turn1.xirsys.com:3478?transport=udp",
-            ],
-          },
-        ],
-      },
-      {
-        iceServers: [
-          {
-            username:
-              "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
-            credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
-            urls: [
-              "turn:ss-turn1.xirsys.com:80?transport=tcp",
-              "turn:ss-turn1.xirsys.com:3478?transport=tcp",
-              "turns:ss-turn1.xirsys.com:443?transport=tcp",
-              "turns:ss-turn1.xirsys.com:5349?transport=tcp",
-            ],
-          },
-        ],
-      },
+      // { iceServers: [] },
+      // {
+      //   iceServers: [
+      //     {
+      //       urls: ["stun:avm4962.com:3478", "stun:avm4962.com:5349"],
+      //     },
+      //     { urls: ["stun:ss-turn1.xirsys.com"] },
+      //   ],
+      // },
+      // {
+      //   iceServers: [
+      //     {
+      //       username: "TuR9Us3r",
+      //       credential:
+      //         "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
+      //       urls: ["turn:avm4962.com:3478", "turn:avm4962.com:5349"],
+      //     },
+      //   ],
+      // },
+      // {
+      //   iceServers: [
+      //     {
+      //       username: "TuR9Us3r",
+      //       credential:
+      //         "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
+      //       urls: [
+      //         "turn:avm4962.com:3478?transport=udp",
+      //         "turn:avm4962.com:5349?transport=udp",
+      //       ],
+      //     },
+      //   ],
+      // },
+      // {
+      //   iceServers: [
+      //     {
+      //       username: "TuR9Us3r",
+      //       credential:
+      //         "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
+      //       urls: [
+      //         "turn:avm4962.com:3478?transport=tcp",
+      //         "turn:avm4962.com:5349?transport=tcp",
+      //       ],
+      //     },
+      //   ],
+      // },
+      // {
+      //   iceServers: [
+      //     {
+      //       username: "TuR9Us3r",
+      //       credential:
+      //         "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
+      //       urls: [
+      //         "turn:avm4962.com:3478?transport=udp",
+      //         "turn:avm4962.com:5349?transport=tcp",
+      //       ],
+      //     },
+      //   ],
+      // },
+      // {
+      //   iceServers: [
+      //     {
+      //       username:
+      //         "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
+      //       credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
+      //       urls: [
+      //         "turn:ss-turn1.xirsys.com:80?transport=udp",
+      //         "turn:ss-turn1.xirsys.com:3478?transport=udp",
+      //       ],
+      //     },
+      //   ],
+      // },
+      // {
+      //   iceServers: [
+      //     {
+      //       username:
+      //         "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
+      //       credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
+      //       urls: [
+      //         "turn:ss-turn1.xirsys.com:80?transport=tcp",
+      //         "turn:ss-turn1.xirsys.com:3478?transport=tcp",
+      //         "turns:ss-turn1.xirsys.com:443?transport=tcp",
+      //         "turns:ss-turn1.xirsys.com:5349?transport=tcp",
+      //       ],
+      //     },
+      //   ],
+      // },
       {
         iceServers: [
           {
@@ -1522,7 +1535,7 @@ class Home extends React.Component {
     } = this.state;
     return (
       <div>
-        <h1>Version 3</h1>
+        <h1>Version 4</h1>
         <h1>{type}</h1>
         <h2>{ip}</h2>
         <h2>
