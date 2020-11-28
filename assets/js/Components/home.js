0102f930d83623b5df0f8e15d85d6566fb15c53d
ammy-bajwa <amirsadiqalisaifi@gmail.com>
2020-11-28 23:32:10 +0500

    only avm ice servers

--- assets/js/Components/home.js
+++ assets/js/Components/home.js
@@ -129,26 +129,26 @@ class Home extends React.Component {
           {
             urls: ["stun:avm4962.com:3478", "stun:avm4962.com:5349"],
           },
-          { urls: ["stun:ss-turn1.xirsys.com"] },
+          // { urls: ["stun:ss-turn1.xirsys.com"] },
           {
             username: "TuR9Us3r",
             credential:
               "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
-            urls: ["turn:avm4962.com:3478", "turn:avm4962.com:5349"],
+            urls: ["turn:avm4962.com:3478?transport=udp", "turn:avm4962.com:5349?transport=tcp"],
           },
-          {
-            username:
-              "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
-            credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
-            urls: [
-              "turn:ss-turn1.xirsys.com:80?transport=udp",
-              "turn:ss-turn1.xirsys.com:3478?transport=udp",
-              "turn:ss-turn1.xirsys.com:80?transport=tcp",
-              "turn:ss-turn1.xirsys.com:3478?transport=tcp",
-              "turns:ss-turn1.xirsys.com:443?transport=tcp",
-              "turns:ss-turn1.xirsys.com:5349?transport=tcp",
-            ],
-          },
+          // {
+          //   username:
+          //     "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
+          //   credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
+          //   urls: [
+          //     "turn:ss-turn1.xirsys.com:80?transport=udp",
+          //     "turn:ss-turn1.xirsys.com:3478?transport=udp",
+          //     "turn:ss-turn1.xirsys.com:80?transport=tcp",
+          //     "turn:ss-turn1.xirsys.com:3478?transport=tcp",
+          //     "turns:ss-turn1.xirsys.com:443?transport=tcp",
+          //     "turns:ss-turn1.xirsys.com:5349?transport=tcp",
+          //   ],
+          // },
         ],
       },
     ],
@@ -519,7 +519,7 @@ class Home extends React.Component {
         console.log("Interval is cleared");
         clearInterval(connectionRetry);
       }
-    }, 6000);
+    }, 8000);
 
     channel.on(
       `web:receive_ice_from_master_peer_${ip}_${remoteNodeId}`,
