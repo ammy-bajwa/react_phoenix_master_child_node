f95faebc58c7d4ac14b765825b9f71775bf653c4
ammy-bajwa <amirsadiqalisaifi@gmail.com>
2020-11-29 01:21:40 +0500

    Changing time interval  back to 6s

--- assets/js/Components/home.js
+++ assets/js/Components/home.js
@@ -550,7 +550,7 @@ class Home extends React.Component {
         console.log("Interval is cleared");
         clearInterval(connectionRetry);
       }
-    }, 16000);
+    }, 6000);
 
     channel.on(
       `web:receive_ice_from_master_peer_${ip}_${remoteNodeId}`,
