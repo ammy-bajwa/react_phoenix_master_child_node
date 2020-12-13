425e48d13cb7a3551444da9f4702f0112f3aa00b
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2020-12-13 23:38:12 +0500

    Updating ice server info

--- assets/js/Components/home.js
+++ assets/js/Components/home.js
@@ -48,10 +48,7 @@ class Home extends React.Component {
             username: "TuR9Us3r",
             credential:
               "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
-            urls: [
-              "turn:avm4962.com:3478?transport=udp",
-              "turn:avm4962.com:5349?transport=udp",
-            ],
+            urls: ["turn:avm4962.com:3478?transport=udp"],
           },
         ],
       },
@@ -62,26 +59,55 @@ class Home extends React.Component {
             username: "TuR9Us3r",
             credential:
               "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
-            urls: [
-              "turn:avm4962.com:3478?transport=tcp",
-              "turn:avm4962.com:5349?transport=tcp",
-            ],
+            urls: ["turn:avm4962.com:5349?transport=udp"],
           },
         ],
       },
-      // 4
-
+      //4
       {
         iceServers: [
           {
             username: "TuR9Us3r",
             credential:
               "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
-            urls: ["turn:avm4962.com:3478", "turn:avm4962.com:5349"],
+            urls: ["turn:avm4962.com:3478?transport=tcp"],
           },
         ],
       },
-      // 5
+      //5
+      {
+        iceServers: [
+          {
+            username: "TuR9Us3r",
+            credential:
+              "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
+            urls: ["turn:avm4962.com:5349?transport=tcp"],
+          },
+        ],
+      },
+      //6
+      {
+        iceServers: [
+          {
+            username: "TuR9Us3r",
+            credential:
+              "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
+            urls: ["turn:avm4962.com:3478"],
+          },
+        ],
+      },
+      //7
+      {
+        iceServers: [
+          {
+            username: "TuR9Us3r",
+            credential:
+              "T!W779M?Vh#5ewJcT=L4v6NcUE*=4+-*fcy+gLAS$^WJgg+wq%?ca^Br@D%Q2MVpyV2sqTcHmUAdP2z4#=S8FAb*3LKGT%W^4R%h5Tdw%D*zvvdWTzSA@ytvEH!G#^99QmW3*5ps^jv@aLdNSfyYKBUS@CJ#hxSp5PRnzP+_YDcJHN&ng2Q_g6Z!+j_3RD%vc@P4g%tFuAuX_dz_+AQNe$$$%w7A4sW?CDr87ca^rjFBGV??JR$!tCSnZdAJa6P8",
+            urls: ["turn:avm4962.com:5349"],
+          },
+        ],
+      },
+      // 8
       {
         iceServers: [
           {
@@ -95,37 +121,73 @@ class Home extends React.Component {
           },
         ],
       },
-      // 6
+      // 9
       {
         iceServers: [
           {
             username:
               "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
             credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
-            urls: [
-              "turn:ss-turn1.xirsys.com:80?transport=udp",
-              "turn:ss-turn1.xirsys.com:3478?transport=udp",
-            ],
+            urls: ["turn:ss-turn1.xirsys.com:80?transport=udp"],
           },
         ],
       },
-      // 7
+      //10
       {
         iceServers: [
           {
             username:
               "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
             credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
-            urls: [
-              "turn:ss-turn1.xirsys.com:80?transport=tcp",
-              "turn:ss-turn1.xirsys.com:3478?transport=tcp",
-              "turns:ss-turn1.xirsys.com:443?transport=tcp",
-              "turns:ss-turn1.xirsys.com:5349?transport=tcp",
-            ],
+            urls: ["turn:ss-turn1.xirsys.com:3478?transport=udp"],
           },
         ],
       },
-      // 8
+      // 11
+      {
+        iceServers: [
+          {
+            username:
+              "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
+            credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
+            urls: ["turn:ss-turn1.xirsys.com:80?transport=tcp"],
+          },
+        ],
+      },
+      //12
+      {
+        iceServers: [
+          {
+            username:
+              "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
+            credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
+            urls: ["turn:ss-turn1.xirsys.com:3478?transport=tcp"],
+          },
+        ],
+      },
+      //13
+      {
+        iceServers: [
+          {
+            username:
+              "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
+            credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
+            urls: ["turns:ss-turn1.xirsys.com:443?transport=tcp"],
+          },
+        ],
+      },
+      //14
+      {
+        iceServers: [
+          {
+            username:
+              "ZyUlEkJOyQDmJFZ0nkKcAKmrrNayVm-rutt8RNHa1EQe_NQADY6Rk4sM2zVstYo_AAAAAF9xt7VhbGl2YXRlY2g=",
+            credential: "820f7cf4-0173-11eb-ad8b-0242ac140004",
+            urls: ["turns:ss-turn1.xirsys.com:5349?transport=tcp"],
+          },
+        ],
+      },
+      // 15
       {
         iceServers: [
           {
@@ -142,7 +204,7 @@ class Home extends React.Component {
           },
         ],
       },
-      // 9
+      // 16
       {
         iceServers: [
           {
@@ -254,6 +316,47 @@ class Home extends React.Component {
     });
   };
 
+  getIceServerType = (controlVariable) => {
+    switch (controlVariable) {
+      case 0:
+        return "0-Null_ICE_SERVER";
+      case 1:
+        return "1-ALL_STUN";
+      case 2:
+        return "2-AVM_TURN_UDP_3478";
+      case 3:
+        return "3-AVM_TURN_UDP_5349";
+      case 4:
+        return "4-AVM_TURN_TCP_3478";
+      case 5:
+        return "5-AVM_TURN_TCP_5349";
+      case 6:
+        return "6-AVM_TURN_3478";
+      case 7:
+        return "7-AVM_TURN_5349";
+      case 8:
+        return "8-AVM_TURN_:3478_UDP:5349_TCP";
+      case 9:
+        return "9-XIRSYS_TURN_UDP_80";
+      case 10:
+        return "10-XIRSYS_TURN_UDP_3478";
+      case 11:
+        return "11-XIRSYS_TURN_TCP_80";
+      case 12:
+        return "12-XIRSYS_TURN_TCP_3478";
+      case 13:
+        return "13-XIRSYS_TURN_TCP_443";
+      case 14:
+        return "14-XIRSYS_TURN_TCP_5349";
+      case 15:
+        return "15-AVM_TURN_UDP_3475_TCP_5349";
+      case 16:
+        return "16-AVM_XIRSYS_STUN_TURN_UDP_TCP_ALL";
+      default:
+        return "None";
+    }
+  };
+
   remotePeerConnectionForMaster = async (
     channel,
     remoteNodeIp,
@@ -270,42 +373,7 @@ class Home extends React.Component {
     );
 
     const updateConnectionType = () => {
-      let iceServerType = "Nothing";
-      switch (iceConfigsControlCounter) {
-        case 0:
-          iceServerType = "0-Null_ICE_SERVER";
-          break;
-        case 1:
-          iceServerType = "1-ALL_STUN";
-          break;
-        case 2:
-          iceServerType = "2-AVM_TURN_UDP_3478_5349";
-          break;
-        case 3:
-          iceServerType = "3-AVM_TURN_TCP_3478_5349";
-          break;
-        case 4:
-          iceServerType = "4-AVM_TURN_3478_5349";
-          break;
-        case 5:
-          iceServerType = "5-AVM_TURN_:3478_UDP:5349_TCP";
-          break;
-        case 6:
-          iceServerType = "6-XIRSYS_TURN_UDP_80_3478";
-          break;
-        case 7:
-          iceServerType = "7-XIRSYS_TURN_TCP_80_3478_443_5349";
-          break;
-        case 8:
-          iceServerType = "8-AVM_STUN_TURN";
-          break;
-        case 9:
-          iceServerType = "9AVM_XIRSYS_STUN_TURN";
-          break;
-        default:
-          iceServerType = "None";
-          break;
-      }
+      let iceServerType = this.getIceServerType(iceConfigsControlCounter);
       const { remoteMasterPeers } = this.state;
       const updatedArr = remoteMasterPeers.map((node) => {
         if (node.machine_id === remoteNodeId) {
@@ -552,7 +620,10 @@ class Home extends React.Component {
               } else {
                 node.totalUnverifiedMessages = 0;
               }
-              node.currentMessage = `${verifyCount}__${remoteNodeId.slice(0, 5)}`;
+              node.currentMessage = `${verifyCount}__${remoteNodeId.slice(
+                0,
+                5
+              )}`;
             }
             return node;
           });
@@ -669,7 +740,10 @@ class Home extends React.Component {
               } else {
                 node.totalVerifiedMessages = totalVerified;
               }
-              node.currentMessage = `${verifyCount}__${remoteNodeId.slice(0, 5)}`;
+              node.currentMessage = `${verifyCount}__${remoteNodeId.slice(
+                0,
+                5
+              )}`;
             }
 
             return node;
@@ -791,42 +865,7 @@ class Home extends React.Component {
       remoteNodeId
     );
     const updateConnectionType = () => {
-      let iceServerType = "Nothing";
-      switch (iceConfigsControlCounter) {
-        case 0:
-          iceServerType = "0-Null_ICE_SERVER";
-          break;
-        case 1:
-          iceServerType = "1-ALL_STUN";
-          break;
-        case 2:
-          iceServerType = "2-AVM_TURN_UDP_3478_5349";
-          break;
-        case 3:
-          iceServerType = "3-AVM_TURN_TCP_3478_5349";
-          break;
-        case 4:
-          iceServerType = "4-AVM_TURN_3478_5349";
-          break;
-        case 5:
-          iceServerType = "5-AVM_TURN_:3478_UDP:5349_TCP";
-          break;
-        case 6:
-          iceServerType = "6-XIRSYS_TURN_UDP_80_3478";
-          break;
-        case 7:
-          iceServerType = "7-XIRSYS_TURN_TCP_80_3478_443_5349";
-          break;
-        case 8:
-          iceServerType = "8-AVM_STUN_TURN";
-          break;
-        case 9:
-          iceServerType = "9AVM_XIRSYS_STUN_TURN";
-          break;
-        default:
-          iceServerType = "None";
-          break;
-      }
+      let iceServerType = this.getIceServerType(iceConfigsControlCounter);
       const { remoteMasterPeers } = this.state;
       const updatedArr = remoteMasterPeers.map((node) => {
         if (node.machine_id === remoteNodeId) {
@@ -1090,42 +1129,7 @@ class Home extends React.Component {
     );
 
     const updateConnectionType = () => {
-      let iceServerType = "Nothing";
-      switch (iceConfigsControlCounter) {
-        case 0:
-          iceServerType = "0-Null_ICE_SERVER";
-          break;
-        case 1:
-          iceServerType = "1-ALL_STUN";
-          break;
-        case 2:
-          iceServerType = "2-AVM_TURN_UDP_3478_5349";
-          break;
-        case 3:
-          iceServerType = "3-AVM_TURN_TCP_3478_5349";
-          break;
-        case 4:
-          iceServerType = "4-AVM_TURN_3478_5349";
-          break;
-        case 5:
-          iceServerType = "5-AVM_TURN_:3478_UDP:5349_TCP";
-          break;
-        case 6:
-          iceServerType = "6-XIRSYS_TURN_UDP_80_3478";
-          break;
-        case 7:
-          iceServerType = "7-XIRSYS_TURN_TCP_80_3478_443_5349";
-          break;
-        case 8:
-          iceServerType = "8-AVM_STUN_TURN";
-          break;
-        case 9:
-          iceServerType = "9AVM_XIRSYS_STUN_TURN";
-          break;
-        default:
-          iceServerType = "None";
-          break;
-      }
+      let iceServerType = this.getIceServerType(iceConfigsControlCounter);
       const { lanPeers } = this.state;
       const updatedArr = lanPeers.map((node) => {
         if (node.machine_id === masterId) {
@@ -1821,42 +1825,7 @@ class Home extends React.Component {
     }, 5000);
 
     const updateConnectionType = () => {
-      let iceServerType = "Nothing";
-      switch (iceConfigsControlCounter) {
-        case 0:
-          iceServerType = "0-Null_ICE_SERVER";
-          break;
-        case 1:
-          iceServerType = "1-ALL_STUN";
-          break;
-        case 2:
-          iceServerType = "2-AVM_TURN_UDP_3478_5349";
-          break;
-        case 3:
-          iceServerType = "3-AVM_TURN_TCP_3478_5349";
-          break;
-        case 4:
-          iceServerType = "4-AVM_TURN_3478_5349";
-          break;
-        case 5:
-          iceServerType = "5-AVM_TURN_:3478_UDP:5349_TCP";
-          break;
-        case 6:
-          iceServerType = "6-XIRSYS_TURN_UDP_80_3478";
-          break;
-        case 7:
-          iceServerType = "7-XIRSYS_TURN_TCP_80_3478_443_5349";
-          break;
-        case 8:
-          iceServerType = "8-AVM_STUN_TURN";
-          break;
-        case 9:
-          iceServerType = "9AVM_XIRSYS_STUN_TURN";
-          break;
-        default:
-          iceServerType = "None";
-          break;
-      }
+      let iceServerType = this.getIceServerType(iceConfigsControlCounter);
       const { lanPeers } = this.state;
       const updatedArr = lanPeers.map((node) => {
         if (node.machine_id === childId) {
