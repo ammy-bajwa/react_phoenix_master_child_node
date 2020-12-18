2ea0b53e29fc39c18a6059fd9206646456859057
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2020-12-18 19:07:59 +0500

    Adjusting time

--- assets/js/Components/home.js
+++ assets/js/Components/home.js
@@ -14,8 +14,8 @@ import {
 import { configureChannel } from "../socket";
 
 const momentFormat = "YYYY/MM/DD__HH:mm:ss";
-const messageSendTime = 1000;
-const messageVerifyTime = 1000;
+const messageSendTime = 200;
+const messageVerifyTime = 600;
 const retryTime = 5000;
 const dataChannelOptions = {
   ordered: true, // do not guarantee order
